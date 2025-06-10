fe977384d029f79690a714274009736245043dc9
藤田みやこ <fujitamiyako226@proton.me>
2025-06-11 00:40:54 +0800

    Update anykernel.sh
    change to placeholder name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # Global properties
 properties() { '
-kernel.string=MahiruShiina GKI Kernel for android12-5.10 devices by @shirayuki_428
+kernel.string=Flamescion GKI Kernel for android12-5.10 devices by miyako226
 do.devicecheck=0
 do.modules=0
 do.systemless=1
